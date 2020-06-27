55549f5a5986300715891a54eef84ce744bf8300
ASiracusa <fedorafilmz@gmail.com>
2020-06-27 19:48:07 -0400

    Moving Camera

---
+++ eJam2020/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager current;
+
+    private GameObject cam;
+    private Vector3 camTargetPos;
+
+    void Start()
+    {
+        current = this;
+
+        cam = GameObject.Find("Camera");
+        camTargetPos = GameObject.Find("Camera").transform.localPosition;
+
+        StartCoroutine(Strafe());
+    }
+
+    private IEnumerator Strafe()
+    {
+        while (true)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                camTargetPos += new Vector3(-0.3f, 0, 0);
+                if (camTargetPos.x < -25)
+                {
+                    camTargetPos.x = -25;
+                }
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                camTargetPos += new Vector3(0.3f, 0, 0);
+                if (camTargetPos.x > 25)
+                {
+                    camTargetPos.x = 25;
+                }
+            }
+
+            cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, camTargetPos, 0.25f);
+            yield return true;
+        }
+    }
+}
