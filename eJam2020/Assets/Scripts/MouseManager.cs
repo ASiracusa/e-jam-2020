fc95982091ed72461ca7ce79e7b23b8b92a20f46
ASiracusa <fedorafilmz@gmail.com>
2020-06-26 21:57:43 -0400

    Kinda Confused with Folders Deleting Themselves

---
+++ eJam2020/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    public static MouseManager current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
