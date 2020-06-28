421332992945766d632549ed93300b46ab0d8388
Maximo <mkbcurb@gmail.com>
2020-06-27 20:54:40 -0400

    Update CustomerManager.cs

--- eJam2020/Assets/Scripts/CustomerManager.cs
+++ eJam2020/Assets/Scripts/CustomerManager.cs
@@ -23,7 +23,7 @@ public class CustomerManager : MonoBehaviour
     public void NewCustomer(){
         totalCustomers-=1;
         errorTol=1;
-        item nextitems = Resources.LoadAll<GameObject>("Items");
+        Item nextitems = Resources.LoadAll<GameObject>("Items");
         var rand = Random.Range(0,nextitems.count-1);
         Item curItem = Instantiate(nextitems[rand], new Vector3(-20, 1, 0), Quaternion.identity);
         items.Add(curItem);
