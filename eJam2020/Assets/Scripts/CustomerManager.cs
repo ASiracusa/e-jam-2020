65b8fa033b3ac7252f03370f5c5a99a1258d46bd
Maximo <mkbcurb@gmail.com>
2020-06-27 20:48:34 -0400

    Update CustomerManager.cs

--- eJam2020/Assets/Scripts/CustomerManager.cs
+++ eJam2020/Assets/Scripts/CustomerManager.cs
@@ -7,23 +7,51 @@ public class CustomerManager : MonoBehaviour
     private int errorTol; //amount of errors to 
     private int totalCustomers;
     public List<Item> items;
+    public CustomerManager current;
+    private static CustomerManager instance = null;
+    private static readonly object padlock = new object();
 
     public CustomerManager(){
         errorTol=1;
+        totalCustomers=2;    
+        NewCustomer();
+        current=Instance;
         totalCustomers=2;
+        current=Instance;
+<<<<<<< HEAD
+=======
         //random items
+>>>>>>> b2abba509a8808dfc08a5bee182fe1190012edbd
     }
+
     public void NewCustomer(){
         totalCustomers-=1;
         errorTol=1;
-        //random items
+        item nextitems = Resources.LoadAll<GameObject>("Items");
+        var rand = Random.range(0,nextitems.count-1);
+        Item curItem = Instantiate(nextitems[rand], new Vector3(-20, 1, 0), Quaternion.identity);
+        items.Add(curItem);
+
     }
+
     public void DeductPoint(){
         if(errorTol<=0){
             //remove points
         }
     }
+
     public void DidError(){
         errorTol-=1;
+    }
+
+        public static CustomerManager Instance{
+        get{ 
+            lock(padlock) {
+                if (instance == null){
+                    instance = new CustomerManager();
+                }
+                return instance;
+            }
+        }
     }
 }
---
+++ eJam2020/Assets/Scripts/PointManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    public PointManager current;
+    private static PointManager instance = null;
+    private static readonly object padlock = new object();
+    public int score;
+
+    public PointManager(){
+        score=10;
+        current=Instance;
+    }
+    
+    public static PointManager Instance{
+        get{ 
+            lock(padlock) {
+                if (instance == null){
+                    instance = new PointManager();
+                }
+                return instance;
+            }
+        }
+    }
+}
